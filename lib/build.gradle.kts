3282d1b790990952e8f454d0242ea1b83a59a63c
Jakob Schäfer <mail@jakobschaefer.de>
2024-11-22 11:56:37 +0100

    chore: publish step for github package repository

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -31,6 +31,16 @@ tasks.named<Test>("test") {
 }
 
 publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url = uri("https://maven.pkg.github.com/JakobSchaefer/ktor-server-htma")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
   publications {
     create<MavenPublication>("htma") {
       groupId = "io.github.jakobschaefer"
