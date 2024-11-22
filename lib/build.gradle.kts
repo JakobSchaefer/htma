e81c4f3ed2ccbef8d743ac468f81b5288a3583b7
Jakob Schäfer <mail@jakobschaefer.de>
2024-11-22 11:40:06 +0100

    chore: removed github publication

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,47 +3,10 @@ plugins {
   alias(libs.plugins.axion)
   id("java-library")
   id("maven-publish")
+  id("signing")
 }
 
 version = scmVersion.version
-
-publishing {
-  repositories {
-    maven {
-      name = "GitHub"
-      url = uri("https://maven.pkg.github.com/JakobSchaefer/ktor-server-htma")
-      credentials {
-        username = project.findProperty("gpr.user") as String?
-        password = project.findProperty("gpr.key") as String?
-      }
-    }
-  }
-  publications {
-    create<MavenPublication>("htma") {
-      groupId = "de.jakobschaefer.htma"
-      artifactId = "ktor-server-htma"
-      version = project.version.toString()
-
-      pom {
-        name = "ktor-server-htma"
-        description = "A web framework driven by HTML and powered by Ktor"
-        licenses {
-          license {
-            name = "MIT License"
-            url = "https://raw.githubusercontent.com/JakobSchaefer/ktor-server-htma/refs/heads/main/LICENSE"
-          }
-        }
-        developers {
-          developer {
-            name = "Jakob Schäfer"
-            email = "mail@jakobschaefer.de"
-          }
-        }
-      }
-      from(components["java"])
-    }
-  }
-}
 
 repositories {
   mavenCentral()
@@ -56,6 +19,8 @@ dependencies {
 }
 
 java {
+  withSourcesJar()
+  withJavadocJar()
   toolchain {
     languageVersion = JavaLanguageVersion.of(21)
   }
@@ -63,4 +28,41 @@ java {
 
 tasks.named<Test>("test") {
   useJUnitPlatform()
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("htma") {
+      groupId = "io.github.jakobschaefer"
+      artifactId = "ktor-server-htma"
+      version = project.version.toString()
+
+      pom {
+        name = "ktor-server-htma"
+        description = "A web framework driven by HTML and powered by Ktor"
+        scm {
+          url = "https://github.com/JakobSchaefer/ktor-server-htma"
+        }
+        licenses {
+          license {
+            name = "MIT License"
+            url = "https://raw.githubusercontent.com/JakobSchaefer/ktor-server-htma/refs/heads/main/LICENSE"
+          }
+        }
+        developers {
+          developer {
+            id = "jakobschaefer"
+            name = "Jakob Schäfer"
+            email = "mail@jakobschaefer.de"
+          }
+        }
+      }
+      from(components["java"])
+    }
+  }
+}
+
+signing {
+  // required properties: signing.keyId, signing.password, signing.secretKeyRingFile
+  sign(publishing.publications["htma"])
 }
