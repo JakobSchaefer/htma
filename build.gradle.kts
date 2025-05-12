a167196297e7a3b5c516f9bc5f78bf49301cbabc
Jakob Schäfer <mail@jakobschaefer.de>
2025-05-12 07:53:57 +0200

    chore: removed axion hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,17 +9,3 @@ plugins {
 }
 
 version = scmVersion.version
-
-scmVersion {
-  hooks {
-    preReleaseHooks.add { context ->
-      val packageFile = file("htma-vite-plugin/package.json")
-      val packageJson = JsonSlurper().parse(packageFile) as MutableMap<String, String>
-      packageJson["version"] = context.releaseVersion
-
-      val updatedJson = JsonOutput.prettyPrint(JsonOutput.toJson(packageJson))
-      packageFile.writeText(updatedJson)
-      println("package.json version updated to ${context.releaseVersion}")
-    }
-  }
-}
