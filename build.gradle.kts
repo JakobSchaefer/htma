f3cc04040e6071620ea611b7537713406c1299bf
Jakob Schäfer <mail@jakobschaefer.de>
2025-05-12 08:08:54 +0200

    chore: example demo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-import org.gradle.kotlin.dsl.add
-
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.dokka)
