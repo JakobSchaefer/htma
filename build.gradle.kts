a48e06b092792001b3ed53534029ce882c0c1a71
Jakob Schäfer <mail@jakobschaefer.de>
2025-05-12 07:49:16 +0200

    chore: moved preReleaseHook into root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import org.gradle.kotlin.dsl.add
+
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.dokka)
@@ -5,3 +9,17 @@ plugins {
 }
 
 version = scmVersion.version
+
+scmVersion {
+  hooks {
+    preReleaseHooks.add { context ->
+      val packageFile = file("htma-vite-plugin/package.json")
+      val packageJson = JsonSlurper().parse(packageFile) as MutableMap<String, String>
+      packageJson["version"] = context.releaseVersion
+
+      val updatedJson = JsonOutput.prettyPrint(JsonOutput.toJson(packageJson))
+      packageFile.writeText(updatedJson)
+      println("package.json version updated to ${context.releaseVersion}")
+    }
+  }
+}
--- htma-vite-plugin/build.gradle.kts
+++ htma-vite-plugin/build.gradle.kts
@@ -1,6 +1,3 @@
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-
 plugins {
   alias(libs.plugins.axion)
   alias(libs.plugins.node)
@@ -9,15 +6,4 @@ plugins {
 version = scmVersion.version
 
 scmVersion {
-  hooks {
-    preReleaseHooks.add { context ->
-      val packageFile = file("package.json")
-      val packageJson = JsonSlurper().parse(packageFile) as MutableMap<String, String>
-      packageJson["version"] = context.releaseVersion
-
-      val updatedJson = JsonOutput.prettyPrint(JsonOutput.toJson(packageJson))
-      packageFile.writeText(updatedJson)
-      println("package.json version updated to ${context.releaseVersion}")
-    }
-  }
 }
